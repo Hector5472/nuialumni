fc78bd79d76ffd19f8feeda03640f31711403c62
Hector5472 <hectormartinn2005@gmail.com>
2026-04-27 10:33:45 +0100

    Formatear código en EjAR.jsx para mejorar la legibilidad

--- src/components/ar/EjAR.jsx
+++ src/components/ar/EjAR.jsx
@@ -1,24 +1,25 @@
 //Ejemplo de un cubo en RA
-import { Canvas} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import XrCube from './XrCube';
 import XrOctahedron from './XrOctahedron';
 import { ARButton, XR } from "@react-three/xr";
 
 
-function EjAR(){
+function EjAR() {
 
 
-    return(
+    return (
         <>
-            <ARButton/>
-            <Canvas>
+            <ARButton />
+            <Canvas
+                style={{ height: '75vh' }} >
                 <XR>
                     <XrOctahedron />
-                   
+
                 </XR>
             </Canvas>
         </>
-)
+    )
 
 }
 
