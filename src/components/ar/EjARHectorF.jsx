95652f5d7f85a5d80b1dec1793921a2f470be987
Hector5472 <hectormartinn2005@gmail.com>
2026-04-27 10:39:53 +0100

    Actualizar posición y campo de visión de la cámara en EjARHectorF

--- src/components/ar/EjARHectorF.jsx
+++ src/components/ar/EjARHectorF.jsx
@@ -7,7 +7,7 @@ function EjARHectorF() {
     return (
         <Canvas
             style={{ height: '100vh' }}           
-            camera={{ position: [0, 0, 5], fov: 2 }} 
+            camera={{ position: [0, -15, 0], fov: 1 }} 
         >
             <ambientLight intensity={1} />        
             <directionalLight position={[5, 5, 5]} intensity={1} /> 
